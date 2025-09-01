ab72a75a0be8be11b117b246f223978fe1366272
yk <yurito163@163.com>
2025-09-01 15:05:56 +0800

    init : 初始化组件工程

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/yurito/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,9 @@
+package com.yurito.middleware.dynamic.thread.pool.sdk.config;
+
+/**
+ * @author Yurito
+ * @description: 动态配置中心入口
+ * @create 2025/9/1 14:49
+ */
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/test/java/com/yurito/middleware/dynamic/thread/pool/ApiTest.java
@@ -0,0 +1,9 @@
+package com.yurito.middleware.dynamic.thread.pool;
+
+/**
+ * @author Yurito
+ * @description: 单元测试
+ * @create 2025/9/1 14:50
+ */
+public class ApiTest {
+}
